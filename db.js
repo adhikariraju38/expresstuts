a1928255aea282a7f5bd50e109b3da7864c79e6a
Raju kumar Yadav <60286414+adhikariraju38@users.noreply.github.com>
2024-05-13 21:10:11 +0545

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const mongoURL =
-  "mongodb+srv://itsmeerajuyadav:DebjHzKCLEOERL6F@lookscout.z4ecoux.mongodb.net/?retryWrites=true&w=majority&appName=lookscout";
+  "";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURL, () => {
