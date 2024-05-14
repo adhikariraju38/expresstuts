30562dc71e34f2a02862c65e1fc1ccf7a1fa2416
Raju kumar Yadav <notrajuyadav@gmail.com>
2024-05-14 17:45:36 +0545

    second class

--- controller/event.js
+++ controller/event.js
@@ -30,3 +30,46 @@ exports.createEvent = async (req,res) =>{
         res.status(500).send("Internal server error occured")
     }
 };
+
+exports.deleteEvent = async(req,res) => {
+    try {
+        let event = Event.findById(req.params.id);
+        if(!event){
+            res.send("No event found");
+        }
+        else{
+            event = await Event.findByIdAndDelete(req.params.id);
+            res.status(200).json({
+                "message":"Data deleted successfully",
+                success:true
+            })
+        }
+    } catch (error) {
+        res.status(500).send("Internal Server error")
+        
+    }
+}
+
+exports.updateEvent = async (req,res) =>{
+    const {title, type, description } = req.body
+    try {
+        const newEvent = {};
+        if(title){newEvent.title = title};
+        if(type){newEvent.type = type};
+        if(description){newEvent.description = description};
+
+        let event = Event.findById(req.params.id);
+        if(!event){
+            res.status(404).send("Data to be updated not found");
+        }
+        else{
+            event = await Event.findByIdAndUpdate(req.params.id, {$set:newEvent},{new:true});
+            res.status(200).json({
+                "message":"Update Successfull",
+                event
+            })
+        }
+    } catch (error) {
+        res.status(500).send("internal server error");   
+    }
+}
--- routes/event.js
+++ routes/event.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getEvents, createEvent } = require("../controller/event");
+const { getEvents, createEvent, deleteEvent,updateEvent } = require("../controller/event");
 
 const router = express.Router();
 
 router.get("/event",getEvents);
 router.post("/event/new", createEvent);
+router.delete("/event/delete/:id", deleteEvent);
+router.put("/event/update/:id", updateEvent);
 
 module.exports = router;
